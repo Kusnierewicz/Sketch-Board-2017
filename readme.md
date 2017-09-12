c74196b3bbd8ab05edda2c0c7611fe74d9edfc26
Kusnierewicz <kusnierewicz@gmail.com>
2017-09-12 23:08:42 +0200

    New version of Sketch Board project developed in 2017

---
+++ readme.md
@@ -0,0 +1,4 @@
+# The Odin Project Assignment: "Build Online Sketch board in JavaScript and JQuery" - 2017 version
+
+... Work in progress ...
+
---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    // sketcher building and listening for mouse movement
+    $('#start').click(function() {
+
+        sketch();
+        // mouse movement = drawing
+        $(".box").on("mouseenter", function() {
+            $(this).addClass('hoovered');
+        });
+    });
+
+    // clearing the sketcher
+    $(".container").on('click', '.clear', function() {
+        $('.hoovered').removeClass('hoovered');
+    });
+
+    // starting with fresh sketcher (reload page)
+    $('.container').on('click', '.new', function() {
+        location.reload();
+    });
+
+    // saving picture by printing it to pdf (web screen)
+    $('.container').on('click', '.save', function() {
+        print($('sketch'));
+    });
+
+});
+
+//sketcher builder
+var sketch = function() {
+    var number = parseInt(document.getElementsByName("sketch_size")[0].value)
+    var number_of_fields = number * number;
+    var dim = 200 + (number * 10) + 'px';
+
+    $('.container').css({ 'width': dim, 'height': dim });
+
+    while (number_of_fields > 0) {
+        $('.sketch').append('<div class="box"</div>');
+        number_of_fields -= 1;
+    };
+};
